e7eccdabc6449bbde9bc78bc73fc4dd26c369073
Cleston Moura <clestonmouractm@gmail.com>
2022-11-25 19:34:53 -0300

    Implementação das funcionalidades de salvar imagens e deletar imagens

--- crud.py
+++ crud.py
@@ -1,35 +1,73 @@
 from sqlalchemy.orm import Session
-import model
-import schema
+from fastapi import UploadFile
+from model import Images
+# import schema
+import os
 
 
 def get_images(db: Session, skip: int = 0, limit: int = 100):
 
-    return db.query(model.Images).offset(skip).limit(limit).all()
+    return db.query(Images).offset(skip).limit(limit).all()
 
 
-def get_image_by_id(db: Session, sl_id: int):
+def add_new_image(db: Session, file: UploadFile):
 
-    return db.query(model.Images).filter(model.Images.id == sl_id).first()
+    file_location = f"images/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        file_object.write(file.file.read())
+
+    img_name = Images(
+        image_name=file.filename
+    )
+    db.add(img_name)
+    db.commit()
+    db.refresh(img_name)
+
+    for value in db.query(Images.id).distinct().order_by(Images.id.desc()).first():
+        name_id = value
+
+    old_name = f"images\\{file.filename}"
+    new_name = f"images\\{name_id}.png"
+
+    os.rename(old_name, new_name)
+
+    return {"status": "image saved"}
 
 
 def delete_image_by_id(db: Session, sl_id: int):
 
     try:
-        db.query(model.Images).filter(model.Images.id == sl_id).delete()
+        db.query(Images).filter(Images.id == sl_id).delete()
         db.commit()
+
+        os.remove(f"images\\{sl_id}.png")
     except Exception as e:
         raise Exception(e)
 
 
-def add_new_image(db: Session, image: schema.ImageAdd):
+def get_image_by_id(db: Session, sl_id: int):
 
-    details = model.Images(
-        image_name=image.image_name,
-        image_hash=image.image_hash,
-        binary=image.binary
-    )
-    db.add(details)
-    db.commit()
-    db.refresh(details)
-    return model.Images(**image.dict())
+    return db.query(Images).filter(Images.id == sl_id).first()
+
+    # def delete_image_by_id(db: Session, sl_id: int):
+
+    #     try:
+    #         db.query(model.Images).filter(model.Images.id == sl_id).delete()
+    #         db.commit()
+    #     except Exception as e:
+    #         raise Exception(e)
+
+    # def add_new_image(db: Session, image: schema.ImageAdd):
+
+    #     details = model.Images(
+    #         image_name=image.image_name,
+    #         image_hash=image.image_hash,
+    #         binary=image.binary
+    #     )
+    #     db.add(details)
+    #     db.commit()
+    #     db.refresh(details)
+
+    #     db.identity_key
+
+    #     return model.Images(**image.dict())
--- schema.py
+++ schema.py
@@ -1,19 +1,13 @@
-from typing import Optional
+# from typing import Optional
 from pydantic import BaseModel
 
 
 class ImageBase(BaseModel):
     image_name: str
-    
 
 
 class ImageAdd(ImageBase):
-    image_hash: str
-    binary: str
-
-
-    class Config:
-        orm_mode = True
+    ...
 
 
 class Image(ImageAdd):
